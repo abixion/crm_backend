229fedf2445388439b5f409bd99a3de751c72dda
ansarkhalil <ansarkhalil93@gmail.com>
2022-09-14 15:57:09 +0500

    jwt implementation with routes middleare

---
+++ middleware/auth.js
@@ -0,0 +1,19 @@
+import jwt from 'jsonwebtoken';
+import { jwtConfig } from '../config/config.js';
+
+const auth = (req, res, next) => {
+  try {
+    if (!req.headers.authorization) {
+      return res.status(403).json({ error: 'Unauthenticated', message: 'token is missing in the request.' });
+    }
+    const token = req.headers.authorization.split(' ')[1];
+    const decodedData = jwt.verify(token, `${jwtConfig.secret}`);
+    // eslint-disable-next-line no-underscore-dangle
+    req.userId = decodedData?._id;
+    next();
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export default auth;
